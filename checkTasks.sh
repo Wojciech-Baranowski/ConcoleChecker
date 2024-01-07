a82051f34a8f81a07f7e07daa02ab8678ff1300b
simmondobber <wojciechfbaranowski@gmail.com>
2024-01-07 22:13:53 +0100

    tests clone

--- checkTasks.sh
+++ checkTasks.sh
@@ -2,6 +2,7 @@
 
 WORKING_DIRECTORY='/tmp/task'
 REPO_NAME='testTaskRepo'
+TEST_REPO_NAME='ConsoleCheckerTests'
 
 while getopts 'n:' FLAG; do
 	case "${FLAG}" in
@@ -12,10 +13,12 @@ done
 rm -rf $WORKING_DIRECTORY
 mkdir $WORKING_DIRECTORY
 cd $WORKING_DIRECTORY
-
 mkdir tasks
+mkdir tests
+
+cd $WORKING_DIRECTORY
 git clone git@github.com:Wojciech-Baranowski/$REPO_NAME.git
-cd testTaskRepo
+cd $REPO_NAME
 BRANCHES=$(git branch --list -a | grep '^\s*remotes/origin/.*[^HEAD|main]$')
 
 for BRANCH in ${BRANCHES[@]}; do 
@@ -24,4 +27,11 @@ for BRANCH in ${BRANCHES[@]}; do
 	BRANCH_OWNER=${BRANCH##*/}
 	gcc $CPP -o $WORKING_DIRECTORY/tasks/$BRANCH_OWNER
 done
+
+cd $WORKING_DIRECTORY
+git clone git@github.com:Wojciech-Baranowski/$TEST_REPO_NAME.git
+cd $TEST_REPO_NAME/$FILE_NAME
+cp -r in $WORKING_DIRECTORY/tests
+cp -r out $WORKING_DIRECTORY/tests
+
 
